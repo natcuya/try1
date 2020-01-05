f45e96c6f08abb01be43071bbfb7e8421ca1a363
Nathaly Cuya <57621005+natcuya@users.noreply.github.com>
2020-01-05 09:13:35 -0600

    Add files via upload

---
+++ STORE.js
@@ -0,0 +1,104 @@
+const STORE = [
+    {//set1
+        question: "What is the Generic name of Augmentin?",
+        answers: [
+           "A. Sumatriptan",
+           "B. Amlodipine",
+           "C. Penicillin",
+          "D. Amoxicillin/Clavulanate",
+              ],
+        correctAnswer: "D. Amoxicillin/Clavulanate" ,
+          }, 
+        { //set2
+          question: "Which is a common antibiotic?",
+          answers: [
+          "A. Zithromax",
+          "B. Androgel",
+          "C. Phentermine",
+          "D. Fluoxetine", 
+          ],
+          correctAnswer: "A. Zithromax",
+        }, 
+        { //set3
+         question: "Which of the following is considered a narcotic drug?",
+          answers: [
+          "A. Duragesic",
+          "B. Misoprostol",
+          "C. Epinephrine",
+          "D. Prozac",
+          ],
+          correctAnswer: "A. Duragesic",
+        },
+        { //set4
+          question: "What is the brand name of Sertraline?",
+          answers: [
+          "A. Synthroid",
+          "B. Lunesta",
+          "C. Zoloft" ,
+          "D. Zestril", 
+          ],
+          correctAnswer: "C. Zoloft",
+        },
+        {//set5
+        question: "What is Cymbalta used for?",
+        answers: [
+          "A. For weight loss",
+          "B. As an antibiotic",
+          "C. Treating depression",
+          "D. As a birth control",
+        ],
+        correctAnswer: "C. Treating depression",
+      },
+      {//set 6
+        question: "What is Actos (pioglitazone) used for?",
+        answers: [
+          "A. To lower blood glucose levels in the treatment of diabetes.",
+          "B. As a diuretic",
+          "C. As an anti-itch cream",
+          "D. As a prenatal",
+        ],
+        correctAnswer: "A. To lower blood glucose levels in the treatment of diabetes.",
+      },
+      {//set7
+        question: "What of the following is a birth control pill?",
+        answers: [
+          "A. Testosterone Cypionate",
+          "B. Ovidrel",
+          "C. Latuda",
+          "D. Lessina",
+        ],
+        correctAnswer: "D. Lessina",
+      },
+      {//set8
+        question: "What of the following is a brand of an inhaler",
+        answers: [
+          "A. Flonase",
+          "B. Ventolin HFA",
+          "C. Risperidone",
+          "D. Effexor",
+        ],
+        correctAnswer: "B. Ventolin HFA",
+      },
+      {//set9
+        question: "What is the generic name of Concerta?",
+        answers: [
+          "A. Citalopram",
+          "B. Methylphenidate",
+          "C. Clonazepam",
+          "D. Atorvastin",
+        ],
+        correctAnswer: "B. Methylphenidate",
+      },
+      {//set10
+        question: "Which of the following is a Schedule III Drug?",
+        answers: [
+          "A. Sudafed",
+          "B. Robitussin",
+          "C. Adderall",
+          "D. Tylenol-Codeine",
+        ],
+        correctAnswer: "D. Tylenol-Codeine",
+      },
+    ];
+    let questionNumber = 1;
+    let score = 0;
